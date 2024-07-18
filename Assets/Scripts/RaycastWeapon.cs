c99b4e2f1725c1e556749469d002dfea5f324616
GiritClap <moonjun210@naver.com>
2024-07-18 22:12:58 +0900

    총 파티클 시스템 제작

--- Assets/Scripts/RaycastWeapon.cs
+++ Assets/Scripts/RaycastWeapon.cs
@@ -6,10 +6,25 @@ public class RaycastWeapon : MonoBehaviour
 {
     public bool isFiring = false;
     public ParticleSystem muzzleFlash;
+    public ParticleSystem hitEffect;
+    public Transform raycastOrigin;
+    public Transform raycastDestination;
+    Ray ray;
+    RaycastHit hitInfo;
     public void StartFire()
     {
         isFiring = true;
         muzzleFlash.Emit(1);
+
+        ray.origin = raycastOrigin.position;
+        ray.direction = raycastDestination.position - raycastOrigin.position;
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1f);
+            hitEffect.transform.position = hitInfo.point;
+            hitEffect.transform.forward = hitInfo.normal;
+            hitEffect.Emit(1);
+        }
     }
 
     public void StopFire()
